9a6dffc176b87980806524aceac29ed185891eb1
SirCremefresh <donato.wolfisberg@gmail.com>
2020-09-19 05:36:43 +0200

    added press to scan button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+import {FormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {WebcamModule} from 'ngx-webcam';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material/material.module';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from "./material/material.module";
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavOverlayComponent } from './components/nav-overlay/nav-overlay.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
+import { CameraCaptureComponent } from './components/camera-capture/camera-capture.component';
 import { environment } from '../environments/environment';
 
 
@@ -17,12 +20,15 @@ import { environment } from '../environments/environment';
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    NavOverlayComponent
+    NavOverlayComponent,
+    CameraCaptureComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    FormsModule,
+    WebcamModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
